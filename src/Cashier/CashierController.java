45cf77ebb13579b7ca32e06db99e3b8f7f7f8835
Keminda Sri Tharaka <keminda4lk@gmail.com>
2018-07-30 17:31:03 +0530

    New Fxml file

--- src/Cashier/CashierController.java
+++ src/Cashier/CashierController.java
@@ -5,11 +5,14 @@
  */
 package Cashier;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * FXML Controller class
@@ -17,13 +20,23 @@ import javafx.fxml.Initializable;
  * @author ASUS
  */
 public class CashierController implements Initializable {
+    @FXML
+private AnchorPane content;
+    
+    
+    
+ 
+
+@FXML
+private void handleButtonAction(ActionEvent event) throws IOException {        
+    
+    
+        content.getChildren().setAll((AnchorPane)FXMLLoader.load(getClass().getResource("/Payment/Payment.fxml")));
+         content.getChildren().setAll((AnchorPane)FXMLLoader.load(getClass().getResource("/Order/Order.fxml")));
+}
 
     
-     @FXML
- private void handleButtonAction(ActionEvent event) {
-     // Button was clicked, do something...
-     System.out.println("abc");
- }
+    
     /**
      * Initializes the controller class.
      */
