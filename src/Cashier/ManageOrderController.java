e7e98a100af2559e5fd149ca66d5654db9d8d0bd
Keminda Sri Tharaka <keminda4lk@gmail.com>
2018-10-14 18:24:20 +0530

    added update order implementation

--- src/Cashier/ManageOrderController.java
+++ src/Cashier/ManageOrderController.java
@@ -5,18 +5,24 @@
  */
 package Cashier;
 
+import Connection.ConnectionManager;
+import java.io.IOException;
 import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -40,28 +46,146 @@ public class ManageOrderController implements Initializable {
     @FXML
     private Button btnUpdate;
 
+    @FXML
+    private TextField queryOrderId;
+
+    @FXML
+    private TextField inputOrderId;
+
+    @FXML
+    private TextField inputOrderItem;
+
+    @FXML
+    private TextField inputQuantity;
+
+    @FXML
+    private TextField inputName;
+
+    @FXML
+    private TextField inputMobile;
+
+    @FXML
+    private TextField inputDate;
+
+    @FXML
+    private TextField inputTime;
+
+    private ConnectionManager connectionManager = new ConnectionManager();
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        updateLayout.setVisible(false);
     }
 
     @FXML
-    private void btnViewOnAction(ActionEvent event) {
-        
+    private void btnViewOnAction(ActionEvent event) throws IOException {
+
     }
 
     @FXML
-    private void btnShowToUpdateOnAction(ActionEvent event) {
+    private void btnShowToUpdateOnAction(ActionEvent event) throws IOException {
+        String OrderID = queryOrderId.getText();
+
+        if (!OrderID.equals("")) {
+
+            try {
+                String query = String.format("SELECT * FROM mydb.order WHERE OrderID=%s", OrderID);
+
+                connectionManager.connect();//connect with the database
+                ResultSet result = connectionManager.executeResults(query);//retrieve the selected result set
+                if (result.next()) {//check whether result is valid or not, if valid get values
+                    updateLayout.setVisible(true);
+                    inputOrderId.setText(result.getString("OrderID"));
+                    inputOrderItem.setText(result.getString("OrderItem"));
+                    inputQuantity.setText(result.getString("Quantity"));
+                    inputName.setText(result.getString("Name"));
+                    inputMobile.setText(result.getString("Mobile"));
+                    inputDate.setText(result.getString("Date"));
+                    inputTime.setText(result.getString("Time"));
+                    ShowMessage("Order Retrieve Status", "Please Update Existing Order");
+                } else {
+                    ShowMessage("Order Retrieve Status", "Invalid Order Id, Please Enter Valid Order ID");
+                }
+                connectionManager.close();
+
+            } catch (SQLException sqlException) {
+                System.err.println(sqlException);
+
+                //sqlException.printStackTrace();
+                ShowMessage("Order Retrieve Status", "Order Loading Failed, Please Try again");
+            } catch (Exception e) {
+                System.err.println(e);
+                ShowMessage("Order Retrieve  Status", "Order Loading Failed, Please Try again");
+            }
+        } else {
+            ShowMessage("Order Retrieve  Status", "Please Enter Order ID and Try again");
+        }
+
     }
 
     @FXML
-    private void btnDeleteOnAction(ActionEvent event) {
+    private void btnDeleteOnAction(ActionEvent event) throws IOException {
     }
 
     @FXML
-    private void btnUpdateOnAction(ActionEvent event) {
+    private void btnUpdateOnAction(ActionEvent event) throws IOException {
+        String QueryOrderID = queryOrderId.getText();
+        String OrderID = inputOrderId.getText();
+        String Order = inputOrderItem.getText();
+        String Quantity = inputQuantity.getText();
+        String Name = inputName.getText();
+        String Mobile = inputMobile.getText();
+        String Date = inputDate.getText();
+        String Time = inputTime.getText();
+
+        try {
+            String query = String.format("UPDATE mydb.order SET OrderID=%s,Order=%s,Quantity=%s,Name=%s,Mobile=%s,Date=%s,Time=%s WHERE OrderID=%s",OrderID,Order,Quantity,Name,Mobile,Date,Time,QueryOrderID);
+
+            connectionManager.connect();
+            connectionManager.execute(query);
+            connectionManager.close();
+            ShowMessage("Update Status", "Updated Successfully");
+            ClearAndHideView();
+        } catch (SQLException sqlException) {
+            System.err.println(sqlException);
+
+            //sqlException.printStackTrace();
+            ShowMessage("Update Status", "Updated Failed, Please Try again");
+        } catch (Exception e) {
+            System.err.println(e);
+            ShowMessage("Update Status", "Updated Failed, Please Try again");
+        }
+    }
+
+    private void showStage(String url) throws IOException {
+        Stage stage1 = new Stage();
+        Parent root1 = FXMLLoader.load(getClass().getResource(url));
+        Scene scene1 = new Scene(root1);
+        stage1.setScene(scene1);
+        stage1.show();
+    }
+
+    private void ShowMessage(String title, String message) {
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle(title);
+        //alert.setHeaderText("Look, an Information Dialog");
+        alert.setContentText(message);
+
+        alert.showAndWait();
+    }
+
+    private void ClearAndHideView() {
+        inputOrderId.setText("");
+        inputOrderItem.setText("");
+        inputQuantity.setText("");
+        inputName.setText("");
+        inputMobile.setText("");
+        inputDate.setText("");
+        inputTime.setText("");
+        updateLayout.setVisible(false);
     }
 }
